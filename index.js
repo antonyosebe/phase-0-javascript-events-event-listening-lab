bfb9cf419730c5288a93e79972f242957ed32f36
antonyosebe <osebe.anto@gmail.com>
2022-11-20 12:19:17 +0300

    <completed assignment>

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+function clickAlert() {
+    return alert('I was clicked!');
+}
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    input.addEventListener('click', clickAlert);
 }
